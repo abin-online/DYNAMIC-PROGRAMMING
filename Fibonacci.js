66338e758a50ad5d1f3e5d03c0a4e1185464b42b
Abin Babu <abinbabuonline@gmail.com>
2024-10-07 13:39:29 +0530

    memoization

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,8 +1,20 @@
-const fib = (n) => {
+ const fib = (n , memo = {}) => {
+    if(n in memo) return memo[n]
     if(n<=2) return 1
-    else return fib(n-2) + fib(n-1)
+    memo[n] = fib(n-2 , memo) + fib(n-1 , memo)
+    return memo[n]
+}  
+
+console.log(fib(769));
+
+
+
+const fibo = (n)=> {
+    if(n<= 2) return 1
+    return fibo(n-1) + fibo(n-2)
 }
 
-console.log(fib(7));
+console.log(fibo(769));
+
 
 //Time complexity : 0(M+N);
